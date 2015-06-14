d66fa7896cf51f75e04a363598327fb96b4f0957
turtlologist <504350677@qq.com>
2015-06-14 17:45:19 +0800

    comment

--- src/main/java/bussinesslogic/playertechbl/transPla.java
+++ src/main/java/bussinesslogic/playertechbl/transPla.java
@@ -3,7 +3,48 @@ package bussinesslogic.playertechbl;
 public class transPla {
 	public String translate(String con){
 		switch(con){
-			case"name":return "";
+			case"name":return "name";
+			case"team":return"team";
+			case"gamenum":return"GP";
+			case"reboundave":return"REB";
+			case"rebound":return"RPG";
+			case"secondaryattackave":return "APG";
+			case"secondaryattack":return"AST";
+			case"timeave":return "MPG";
+			case"time":return "MIN";
+			case"shotinrate":return"FG%";
+			case"threeshotinrate":return"3P%";
+			case"penaltyshotinrate":return"FT%";
+			case"offensivenumave":return"ORPG";
+			case"offensivenum":return"OFF";
+			case"defensivenumave":return"DRPG";
+			case"defensivenum":return"DEF";
+			case"blockshotave":return"BLKPG";
+			case"blockshot":return"BLK";
+			case"faultave":return"TOPG";
+			case"fault":return"TO";
+			case"foulave":return"PF";
+			case"foul":return"PFPG";
+			case"score":return"PTS";
+
+			case"trueshotinrate":return"TS";
+			case"reboundrate":return"REBR";
+			case"offensivereboundrate":return"ORR";
+			case"defensivereboundrate":return"DRR";
+			case"secondaryattackrate":return"AST";
+			case"faultrate":return"TO";
+			case"usagerate":return"USG";
+			
+			case"scoreave":return"PTS";
+			
+			case"efficiency":return"";
+			
+			
+			case"blockshotrate":return"blockshotrate";
+			case"stealrate":return"stealrate";
+			case"shootingefficiency":return"shootingefficiency";
+			case"gmscefficiency":return"GMSC";
+			case"startingnum":return"start";
 			default: return null;
 		}
 	}
