62e0320b6210dc1601cb5d0535f25f62c2d22370
turtlologist <504350677@qq.com>
2015-06-15 16:23:45 +0800

    commit

--- src/main/java/bussinesslogic/playertechbl/findPlayerTech.java
+++ src/main/java/bussinesslogic/playertechbl/findPlayerTech.java
@@ -40,6 +40,7 @@ public class findPlayerTech implements FindPlayerTechService{
 	@Override
 	public ArrayList<PlayerTechVO> findFastImprovingPlayer(String keyword) {
 		ArrayList<PlayerTechVO> result = new ArrayList<PlayerTechVO>();
+		if(keyword.equals("score")){
 		PlayerTechVO ptvo = new PlayerTechVO();
 		PlayerTechVO ptvo1 = new PlayerTechVO();
 		PlayerTechVO ptvo2 = new PlayerTechVO();
@@ -75,7 +76,161 @@ public class findPlayerTech implements FindPlayerTechService{
 		result.add(ptvo1);
 		result.add(ptvo2);
 		result.add(ptvo3);
-		result.add(ptvo4);
+		result.add(ptvo4);}
+		else if(keyword.equals("rebound")){
+		PlayerTechVO ptvo = new PlayerTechVO();
+		PlayerTechVO ptvo1 = new PlayerTechVO();
+		PlayerTechVO ptvo2 = new PlayerTechVO();
+		PlayerTechVO ptvo3 = new PlayerTechVO();
+		PlayerTechVO ptvo4 = new PlayerTechVO();
+		
+		ptvo.name = "Boris Diaw";
+		ptvo.team = "SAS";
+		ptvo.rebound = 120;
+		ptvo.reboundImproving = 0.046;
+		
+		ptvo1.name = "Alonzo Gee";
+		ptvo1.team = "CLE";
+		ptvo1.rebound = 127;
+		ptvo1.reboundImproving = 0.044;
+		
+		ptvo2.name = "Tristan Thompson";
+		ptvo2.team = "CLE";
+		ptvo2.rebound = 127;
+		ptvo2.reboundImproving = 0.044;
+		
+		ptvo3.name = "Luis Scola";
+		ptvo3.rebound = 127;
+		ptvo3.team = "PHX";
+		ptvo3.reboundImproving = 0.044;
+		
+		ptvo4.name = "Marcin Gortat";
+		ptvo4.rebound = 127;
+		ptvo4.reboundImproving = 0.044;
+		ptvo4.team = "PHX";
+		
+		result.add(ptvo);
+		result.add(ptvo1);
+		result.add(ptvo2);
+		result.add(ptvo3);
+		result.add(ptvo4);}
+		
+		else if(keyword.equals("secondaryattack")){
+		PlayerTechVO ptvo = new PlayerTechVO();
+		PlayerTechVO ptvo1 = new PlayerTechVO();
+		PlayerTechVO ptvo2 = new PlayerTechVO();
+		PlayerTechVO ptvo3 = new PlayerTechVO();
+		PlayerTechVO ptvo4 = new PlayerTechVO();
+		
+		ptvo.name = "Mario Chalmers";
+		ptvo.team = "MIA";
+		ptvo.secondaryAttack = 104;
+		ptvo.secondaryAttackImproving = 1.238;
+		
+		ptvo1.name = "Luke Ridnour";
+		ptvo1.team = "MIN";
+		ptvo1.secondaryAttack = 118;
+		ptvo1.secondaryAttackImproving = 0.958;
+		
+		ptvo2.name = "Sebastian Telfair";
+		ptvo2.team = "PHX";
+		ptvo2.secondaryAttack = 87;
+		ptvo2.secondaryAttackImproving = 0.718;
+		
+		ptvo3.name = "Jared Dudley";
+		ptvo3.secondaryAttack = 82;
+		ptvo3.team = "PHX";
+		ptvo3.secondaryAttackImproving = 0.692;
+		
+		ptvo4.name = "D.J. Augustin";
+		ptvo4.secondaryAttack = 63;
+		ptvo4.secondaryAttackImproving = 0.620;
+		ptvo4.team = "PHX";
+		
+		result.add(ptvo);
+		result.add(ptvo1);
+		result.add(ptvo2);
+		result.add(ptvo3);
+		result.add(ptvo4);}
+		
+		
+		else if(keyword.equals("blockshot")){
+		PlayerTechVO ptvo = new PlayerTechVO();
+		PlayerTechVO ptvo1 = new PlayerTechVO();
+		PlayerTechVO ptvo2 = new PlayerTechVO();
+		PlayerTechVO ptvo3 = new PlayerTechVO();
+		PlayerTechVO ptvo4 = new PlayerTechVO();
+		
+		ptvo.name = "Joakim Noah";
+		ptvo.team = "CHI";
+		ptvo.blockShot = 61;
+		ptvo.blockShotImproving = 0.614;
+		
+		ptvo1.name = "Kendick Perkins";
+		ptvo1.team = "OKC";
+		ptvo1.blockShot = 31;
+		ptvo1.blockShotImproving = 0.54;
+		
+		ptvo2.name = "Serge Ibaka";
+		ptvo2.team = "OKC";
+		ptvo2.blockShot = 89;
+		ptvo2.blockShotImproving = 0.515;
+		
+		ptvo3.name = "Marc Gasol";
+		ptvo3.blockShot = 49;
+		ptvo3.team = "MEM";
+		ptvo3.blockShotImproving = 0.404;
+		
+		ptvo4.name = "Ian Mahinmi";
+		ptvo4.blockShot = 29;
+		ptvo4.blockShotImproving =0.354;
+		ptvo4.team = "IND";
+		
+		result.add(ptvo);
+		result.add(ptvo1);
+		result.add(ptvo2);
+		result.add(ptvo3);
+		result.add(ptvo4);}
+		
+		
+		else if(keyword.equals("steal")){
+		PlayerTechVO ptvo = new PlayerTechVO();
+		PlayerTechVO ptvo1 = new PlayerTechVO();
+		PlayerTechVO ptvo2 = new PlayerTechVO();
+		PlayerTechVO ptvo3 = new PlayerTechVO();
+		PlayerTechVO ptvo4 = new PlayerTechVO();
+		
+		ptvo.name = "Jeremy Lin";
+		ptvo.team = "HOU";
+		ptvo.steal = 58;
+		ptvo.stealImproving = 0.874;
+		
+		ptvo1.name = "LeBron James";
+		ptvo1.team = "MIA";
+		ptvo1.steal = 45;
+		ptvo1.stealImproving = 0.71;
+		
+		ptvo2.name = "Joakim Noah";
+		ptvo2.team = "CHI";
+		ptvo2.steal = 41;
+		ptvo2.stealImproving = 0.614;
+		
+		ptvo3.name = "Jame Harden";
+		ptvo3.steal = 55;
+		ptvo3.team = "HOU";
+		ptvo3.stealImproving = 0.54;
+		
+		ptvo4.name = "Alonzo Gee";
+		ptvo4.steal = 45;
+		ptvo4.stealImproving = 0.422;
+		ptvo4.team = "CLE";
+		
+		result.add(ptvo);
+		result.add(ptvo1);
+		result.add(ptvo2);
+		result.add(ptvo3);
+		result.add(ptvo4);}
+		
 		
 		return result;
 	}
