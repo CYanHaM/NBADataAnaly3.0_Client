cfb7271e093a306b6fdfde45be827bfd868f655d
turtlologist <504350677@qq.com>
2015-06-15 16:44:58 +0800

    commit

--- src/main/java/bussinesslogic/teamTech/translate.java
+++ src/main/java/bussinesslogic/teamTech/translate.java
@@ -5,53 +5,53 @@ import TypeEnum.TeamTechEnum;
 public class translate {
 	public String translate(TeamTechEnum en){
 		switch(en){
-			case name :return "team";
-			case shotInNum:return "FGM";
-			case shotNum:return "FGA";
-			case threeShotInNum:return "TPM";
-			case threeShotNum:return "TPA";
-			case penaltyShotInNum:return "FTM";
-			case penaltyShotNum:return "FTA";
-			case offensiveRebound:return "OffenOWN";
-			case defensiveRebound:return "DefenOWN";
-			case rebound:return "TOTOWN";
-			case secondaryAttack:return "ASS_OWN";
-			case steal:return "STE_OWN";
-			case blockShot:return "BLO_OWN";
-			case fault:return "TUR_OWN";
-			case foul:return "TUR_TECH";
-			case score:return "OWN_Poi";
-			case shotInRate:return "OWN_FG";
-			case threeShotInRate:return "OWN_3P";
-			case penaltyShotInRate:return "FT";
-			case offensiveEfficiency:return "off_EFF";
-			case defensiveEfficiency:return "def_eff";
+			case name :return "name";
+			case shotInNum:return "shotInNum";
+			case shotNum:return "shotNum";
+			case threeShotInNum:return "threeShotInNum";
+			case threeShotNum:return "threeShotNum";
+			case penaltyShotInNum:return "penaltyShotInNum";
+			case penaltyShotNum:return "penaltyShotNum";
+			case offensiveRebound:return "offensiveRebound";
+			case defensiveRebound:return "defensiveRebound";
+			case rebound:return "rebound";
+			case secondaryAttack:return "secondaryAttack";
+			case steal:return "steal";
+			case blockShot:return "blockShot";
+			case fault:return "fault";
+			case foul:return "foul";
+			case score:return "score";
+			case shotInRate:return "shotInRate";
+			case threeShotInRate:return "threeShotInRate";
+			case penaltyShotInRate:return "penaltyShotInRate";
+			case offensiveEfficiency:return "offensiveEfficiency";
+			case defensiveEfficiency:return "defensiveEfficiency";
 			
-			case shotInNumave:return"FGM";
-			case threeShotInNumave:return"TPM";
-			case shotNumave:return"FGA";
-			case threeShotNumave:return"TPA";
-			case penaltyShotInNumave:return"FTM";
-			case penaltyShotNumave:return "FTA";
-			case offensiveReboundave:return "OffenOWN";
-			case defensiveReboundave:return "DefenOWN";
-			case reboundave:return "TOTOWN";
+			case shotInNumave:return"shotInNum";
+			case threeShotInNumave:return"threeShotInNum";
+			case shotNumave:return"shotNum";
+			case threeShotNumave:return"threeShotNum";
+			case penaltyShotInNumave:return"penaltyShotInNum";
+			case penaltyShotNumave:return "penaltyShotNum";
+			case offensiveReboundave:return "offensiveRebound";
+			case defensiveReboundave:return "defensiveRebound";
+			case reboundave:return "rebound";
 			case secondaryAttackave:return"ASS_OWN";
-			case stealave:return"STE_OWN";
-			case blockShotave:return "BLO_OWN";
-			case faultave:return"TUR_OWN";
-			case foulave:return"TUR_TECH";
-			case scoreave:return"OWN_Poi";
+			case stealave:return"steal";
+			case blockShotave:return "blockShot";
+			case faultave:return"fault";
+			case foulave:return"foul";
+			case scoreave:return"score";
 
 			
 			
-			case winningNum:return "Win_NUM";
-			case winningRate:return "Win_Rate";
-			case reboundEfficiency:return "reb_eff";
-			case stealEfficiency:return "ste_eff";
-			case secondaryAttackEfficiency:return "sec_eff";
-			case offensiveRound:return "round";
-			case offensiveRoundave:return"round";
+			case winningNum:return "winningNum";
+			case winningRate:return "winningRate";
+			case reboundEfficiency:return "reboundEfficiency";
+			case stealEfficiency:return "stealEfficiency";
+			case secondaryAttackEfficiency:return "secondaryAttackEfficiency";
+			case offensiveRound:return "offensiveRound";
+			case offensiveRoundave:return"offensiveRound";
 			default: return null;
 		}
 	}
